81889b8cae254019fcc7f8d969fd2dc5309a368a
Dave Voutila <voutilad@gmail.com>
2018-03-30 18:00:19 -0400

    Cleaner Spotfire Web Player component. Still some flakiness.

--- src/components/SpotfireWebPlayer.js
+++ src/components/SpotfireWebPlayer.js
@@ -1,86 +1,77 @@
-import React from 'react'
+import React from 'react';
+import spotfire from '../lib/spotfire-7.5';
 
-import spotfire from '../lib/spotfire-7.5'
 
+type SpotfireWebPlayerProps = {
+  host: string;
+  file: string;
+}
 
-const host = "http://crspotfire191.pkiapps.net:443/spotfire/wp/"
-const file = "/User Demos/Gerard Conway/worldbank"
-
+const DEFAULT_HOST = 'http://crspotfire191.pkiapps.net:443/spotfire/wp/';
+const DEFAULT_FILE = '/User Demos/Gerard Conway/worldbank';
 
 /**
  * Embeds a Spotfire WebPlayer using Spotfire JS library!
  */
-class SpotfireWebPlayer extends React.Component<> {
+class SpotfireWebPlayer extends React.Component<SpotfireWebPlayerProps> {
+  static defaultProps: SpotfireWebPlayerProps = {
+    host: DEFAULT_HOST,
+    file: DEFAULT_FILE,
+  };
 
   constructor(props) {
-    super(props)
-    this.state = { msg: "Loading..." }
-    this.doc = {}
-    this.app = {}
-    this.on = false
+    super(props);
+    this.state = {
+      msg: 'Initializing Spotfire Web Player...',
+      isLoaded: false,
+      isInitializing: true,
+    };
+    const app = new spotfire.webPlayer.Application(this.props.host);
 
-    this.errorCallback = this.errorCallback.bind(this);
-    this.onClosedCallback = this.onClosedCallback.bind(this);
-  }
-
-  onClosedCallback(errorCode, description) {
-    console.log("Spotfire Application closed.")
-
-    delete this.state.app
-
-    this.setState({ msg: "App closed", doc: {}, app: {}, on: false });
-    //console.log(description);
-  }
-
-  errorCallback(errorCode, description) {
-    try {
-      this.app.close();
-    } catch (err) {
-      console.log("failed to close app")
-    }
-    this.setState({ msg: description, doc: {}, app: {}, on: false });
+    app.onError(this.errorCallback.bind(this));
+    app.onClosed(this.onClosedCallback.bind(this));
+    app.onOpened(this.onOpenedCallback.bind(this));
+    this.app = app;
   }
 
   componentDidMount() {
-    console.log("SpotfireWebPlayer: didMount")
-
-    let settings = new spotfire.webPlayer.Customization();
-    settings.showStatusBar = true;
-    settings.showToolBar = true;
-    settings.showPageNavigation = true;
-
-    var app = new spotfire.webPlayer.Application(host, settings, file);
-    app.onError(this.errorCallback);
-    app.onClosed(this.onClosedCallback);
-
-    var doc = app.openDocument("container");
-    doc.getActivePage((page) => {
-      this.state.msg = page.pageTitle;
-    });
-
-    this.setState({doc: doc, app: app, on: true, msg: "Loaded."});
-    console.log("SpotfireWebPlayer: done didMount");
+    console.log(`Opening app { host: "${this.props.host}", file: "${this.props.file}" }`);
+    this.app.open(this.props.file, 'container');
   }
 
   componentWillUnmount() {
-    console.log("SpotfireWebPlayer: willUnmount");
+    console.log('unmount called!');
 
-    //this.state.app.close();
+    if (this.app) {
+      this.app.close();
+    }
+  }
 
+  onOpenedCallback() {
+    this.setState({ msg: 'Spotfire doc opened!', isLoaded: true, isInitializing: false });
+  }
+
+  onClosedCallback(path) {
+    this.setState({ msg: `Document at path "${path}" closed.`, isLoaded: false });
+  }
+
+  errorCallback(error) {
+    console.error(`Spotfire error: ${error}`);
+    this.setState({ msg: error, isLoaded: false, isInitializing: false });
   }
 
   render() {
-    var spotfireStyle = {height: "300"};
+    let spotfireStyle = { height: 350, display: 'none' };
 
-    if (!this.state.on) {
-      spotfireStyle = {display: "none"}
+    if (this.state.isLoaded || this.state.isInitializing) {
+      spotfireStyle = { height: 350, display: 'block' };
     }
 
     return (
       <div>
-        <h3>Spotfire Player</h3>
+        <h3>Spotfire Web Player</h3>
         <div>{this.state.msg}</div>
-        <div id="container" style={spotfireStyle} ref={el => this.el = el}/>
+        <div id="container" style={spotfireStyle} />
       </div>
     );
   }
